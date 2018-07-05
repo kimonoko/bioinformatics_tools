f9bcf1bf1d97e72bab74356d3fcf96b01b44953f
kimonoko <8824234+kimonoko@users.noreply.github.com>
2018-07-05 12:47:57 +0100

    Viral titer using BFP-induced infectivity
    Prompts user to submit the necessary values so that it can output the following formula for viral titer calculation:
    
    titer = {(F × Cn)/V} × DF
    F: The frequency of GFP-positive cells determined by flow cytometry; Cn: The total number of target cells infected.
    V: The volume of the inoculum.
    DF: The virus dilution factor.

--- BFP_Viral_Titer_CalculatorV1.py
+++ BFP_Viral_Titer_CalculatorV1.py
@@ -5,13 +5,13 @@ class style:
 from decimal import Decimal
 
 print("Welcome to the BFP Viral Titer Calculator.")
-
+print("\n")
 #Before proceeding, you should have the following information ready: the initial number of cells you plated, the number of cells you analyzed for BFP and the number of BFP positive cells for each viral diultion.
 
 Initial_Cells = int(input(style.BOLD + "Please input the initial cell count for all plates. " + style.END))
-
+print("\n")
 Cells_Analyzed = int(input(style.BOLD + "Please input the number of cells analyzed per sample. " + style.END))
-
+print("\n")
 One_To_Fifty = int(input(style.BOLD + "Please input the BFP+ cell count for the 1:50 viral dilution. " + style.END))
 
 f = ((One_To_Fifty / Cells_Analyzed) * Initial_Cells) * 50
@@ -45,3 +45,64 @@ print(style.BOLD + "\nThe viral titer of the 1:10000 sample is", '%.2E' % Decima
 import numpy
 Average_Titer = numpy.average([f, h, x, y, t, u])
 print(style.BOLD + "\nThe average viral titer of this library is", '%.2E' % Decimal(Average_Titer), "TU/mL\n" + style.END)
+
+titers = [f, h, x, y, t, u]
+
+#Determine IQR
+import pandas as pd
+import numpy as np
+from scipy.stats import iqr
+x = np.array(titers)
+x
+Interquartile_Range = iqr(x)
+Sci_Interquartile_Range = "%.2E" % Decimal(Interquartile_Range)
+
+print(style.BOLD + "The interquartile range for this data is", Sci_Interquartile_Range + style.END)
+
+Q1 = np.percentile(titers, 25)
+Median = np.percentile(titers, 50)
+Q3 = np.percentile(titers, 75)
+
+Lower_Inner_Fence = Q1 - (Interquartile_Range * 1.5)
+Upper_Inner_Fence = Q3 + (Interquartile_Range * 1.5)
+
+Lower_Outer_Fence = Q1 - (Interquartile_Range * 3)
+Upper_Outer_Fence = Q3 + (Interquartile_Range * 3)
+
+print("\n")
+print(style.BOLD + "According to this interquartile range:" + style.END)
+print("\n")
+for titer in titers:
+    if titer > Lower_Inner_Fence and titer < Upper_Inner_Fence and titer > Lower_Outer_Fence and titer < Upper_Outer_Fence:
+        print(str("%.2E" % Decimal(titer)), "is not a major or minor outlier.")
+    elif titer < Lower_Outer_Fence or titer > Upper_Outer_Fence:
+        print(style.BOLD + str("%.2E" % Decimal(titer)), "is a major outlier." + style.END)
+    elif titer < Lower_Inner_Fence or titer > Upper_Inner_Fence:
+        print(style.BOLD + str("%.2E" % Decimal(titer)), "is a minor outlier." + style.END)
+
+#Determine Modified Z-scores
+Mean = np.average(titers)
+Median = np.median(titers)
+#MAD Step 1 - subtract median from each value and make new list
+DiffList = [np.abs(i - Median) for i in titers]
+#MAD Step 2 - Find the absolute value of the median of that list
+MAD = np.median(DiffList)
+StdDev = np.std(titers, dtype=np.float64)
+Const = .6745
+
+ModZ = [(Const * (i - Median))/MAD for i in titers]
+ModZ2 = ["%.2f" % round(l,2) for l in ModZ]
+ModZ3 = [float(f) for f in ModZ2]
+print("\n")
+print(style.BOLD + "The modified Z-scores for this data are:" + style.END)
+print("\n")
+print(ModZ3)
+print("\n")
+print(style.BOLD + "According to the modified Z-scores for this data (cutoff |3.5|):" + style.END)
+print("\n")
+
+for z in ModZ:
+    if np.abs(z) > 3.5:
+        print(style.BOLD + str("%.2f" % round(z,2)), "is an outlier." + style.END)
+    else:
+        print(str("%.2f" % round(z,2)), "is not an outlier.")
