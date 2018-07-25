7d3be88ed71279e2cbb2d90b540e1e3f8b5a679e
kimonoko <8824234+kimonoko@users.noreply.github.com>
2018-07-25 16:20:40 +0100

    Formatting fixes

--- Determine_Modified_Z-scores.py
+++ Determine_Modified_Z-scores.py
@@ -4,14 +4,16 @@ class style:
    BOLD = '\033[1m'
    END = '\033[0m'
 
-#Determine Modified Z-scores
+# Determine Modified Z-scores
 titers = [7.29E+08, 6.80E+06, 5.44E+06, 4.20E+06, 3.99E+06, 6.65E+06]
 
 Mean = np.average(titers)
 Median = np.median(titers)
-#MAD Step 1 - subtract median from each value and make new list
+
+# MAD Step 1 - subtract median from each value and make new list
 DiffList = [np.abs(i - Median) for i in titers]
-#MAD Step 2 - Find the absolute value of the median of that list
+
+# MAD Step 2 - Find the absolute value of the median of that list
 MAD = np.median(DiffList)
 StdDev = np.std(titers, dtype=np.float64)
 Const = .6745
