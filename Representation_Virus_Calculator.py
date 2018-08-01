e1eefd752077f947947e1e7cc09675efc26eea9b
kimonoko <8824234+kimonoko@users.noreply.github.com>
2018-08-01 13:49:07 +0100

    CRISPR Screen Viral Calculator
    Calculates the volume of virus needed per plate in a CRISPR screen assuming the user inputs representation they want to maintain, the titer of the library and other experimental conditions.

---
+++ Representation_Virus_Calculator.py
@@ -0,0 +1,21 @@
+from decimal import Decimal
+
+class style:
+    BOLD = '\033[1m'
+    END = '\033[0m'
+
+Num_Guides = int(input(style.BOLD + "Please input the number of guides in your library. " + style.END))
+Rep = float(input(style.BOLD + "Please input the representation you wish to keep in the screen. " + style.END))
+MOI = float(input(style.BOLD + "Please input the target MOI. " + style.END))
+PlatesPerCond = int(input(style.BOLD + "Please input the number of plates per condition. " + style.END))
+Titer = float(input(style.BOLD + "Please input the titer of the library. " + style.END))
+
+print("\n")
+PartPerCond = Num_Guides * Rep
+CellPerPlate = (PartPerCond / MOI) / PlatesPerCond
+
+PartPerPlate = CellPerPlate * MOI
+VolPerPlate = (PartPerPlate / Titer) * 1000
+
+print("You will need", style.BOLD + str('%.2E' % Decimal(PartPerCond)), "viral particles per condition" + style.END, "and", style.BOLD +  str('%.2E' % Decimal(CellPerPlate)), "cells per plate" + style.END, "to maintain", style.BOLD + str(Rep) + "X representation." + style.END)
+print("To achieve", style.BOLD + str('%.2E' % Decimal(PartPerPlate)), "viral particles per plate" + style.END, "for an MOI of", str(MOI) + ", you will need to use", style.BOLD + str(VolPerPlate) + "μL of virus." + style.END)
