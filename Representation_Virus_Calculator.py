ecaf46c5ff8ca536d26011a0a16d67222e76d6fa
kimonoko <8824234+kimonoko@users.noreply.github.com>
2018-08-01 14:03:20 +0100

    Formatting of final results

--- Representation_Virus_Calculator.py
+++ Representation_Virus_Calculator.py
@@ -5,7 +5,7 @@ class style:
     END = '\033[0m'
 
 Num_Guides = int(input(style.BOLD + "Please input the number of guides in your library. " + style.END))
-Rep = float(input(style.BOLD + "Please input the representation you wish to keep in the screen. " + style.END))
+Rep = int(input(style.BOLD + "Please input the representation you wish to keep in the screen. " + style.END))
 MOI = float(input(style.BOLD + "Please input the target MOI. " + style.END))
 PlatesPerCond = int(input(style.BOLD + "Please input the number of plates per condition. " + style.END))
 Titer = float(input(style.BOLD + "Please input the titer of the library. " + style.END))
@@ -17,5 +17,5 @@ CellPerPlate = (PartPerCond / MOI) / PlatesPerCond
 PartPerPlate = CellPerPlate * MOI
 VolPerPlate = (PartPerPlate / Titer) * 1000
 
-print("You will need", style.BOLD + str('%.2E' % Decimal(PartPerCond)), "viral particles per condition" + style.END, "and", style.BOLD +  str('%.2E' % Decimal(CellPerPlate)), "cells per plate" + style.END, "to maintain", style.BOLD + str(Rep) + "X representation." + style.END)
-print("To achieve", style.BOLD + str('%.2E' % Decimal(PartPerPlate)), "viral particles per plate" + style.END, "for an MOI of", str(MOI) + ", you will need to use", style.BOLD + str(VolPerPlate) + "μL of virus." + style.END)
+print("You will need", style.BOLD + str('%.2E' % Decimal(PartPerCond)), "viral particles per condition" + style.END, "and", style.BOLD +  str('%.2E' % Decimal(CellPerPlate)), "cells per plate" + style.END, "to maintain", str(Rep) + "X representation.")
+print("To achieve", style.BOLD + str('%.2E' % Decimal(PartPerPlate)), "viral particles per plate" + style.END, "for an MOI of", str(MOI) + ", you will need to use", style.BOLD + str("%.2f" % VolPerPlate), "μL of virus." + style.END)
