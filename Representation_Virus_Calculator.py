3bb318f0d7419f2fd63aa05cf9227682a67819b3
kimonoko <8824234+kimonoko@users.noreply.github.com>
2018-09-20 10:36:45 +0100

    Updated formatting of output.

--- Representation_Virus_Calculator.py
+++ Representation_Virus_Calculator.py
@@ -5,9 +5,9 @@ class style:
     END = '\033[0m'
 
 Num_Guides = int(input(style.BOLD + "Please input the number of guides in your library. " + style.END))
-Rep = int(input(style.BOLD + "Please input the representation you wish to keep in the screen. " + style.END))
+Rep = int(input(style.BOLD + "Please input the representation you wish to keep in the screen (X). " + style.END))
 MOI = float(input(style.BOLD + "Please input the target MOI. " + style.END))
-Titer = float(input(style.BOLD + "Please input the titer of the library. " + style.END))
+Titer = float(input(style.BOLD + "Please input the titer of the library (TU/mL). " + style.END))
 
 print("\n")
 PartPerCond = Num_Guides * Rep
@@ -15,5 +15,5 @@ CellPerCond = (PartPerCond / MOI)
 
 VolPerCond = (PartPerCond / Titer) * 1000
 
-print("To achieve an MOI of " + str(MOI) + " and " + str(Rep) + "X representation, you will need to infect " + str('%.2E' % Decimal(CellPerCond)) + " cells with " + str('%.2E' % Decimal(PartPerCond)) + " viral particles per condition." + style.END)
-print("Based on the titer of this library, this means you will need to use " + str("%.2f" % VolPerCond), "μL of virus.")
+print("To achieve an MOI of " + style.BOLD + str(MOI) + style.END + " and " + style.BOLD +  str(Rep) + "X representation, " + style.END + "you will need to infect " + style.BOLD + str('%.2E' % Decimal(CellPerCond)) + " cells " + style.END + "with " + style.BOLD + str('%.2E' % Decimal(PartPerCond)) + " viral particles per condition." + style.END + "\n")
+print("Based on the titer of this library, this means you will need to use " + style.BOLD + str("%.2f" % VolPerCond), "μL of virus." + style.END)
